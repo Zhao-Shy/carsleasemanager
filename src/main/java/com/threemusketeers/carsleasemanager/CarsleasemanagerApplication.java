fe3f312f1326d75674ff9d57be161700fb5f59b1
zhaoshy <352807019@qq.com>
2020-01-17 22:15:35 +0800

    框架搭建

---
+++ src/main/java/com/threemusketeers/carsleasemanager/CarsleasemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.threemusketeers.carsleasemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsleasemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsleasemanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threemusketeers/carsleasemanager/CarsleasemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threemusketeers.carsleasemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsleasemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
